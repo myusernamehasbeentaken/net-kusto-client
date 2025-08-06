bac10e6ec6fdb4a7f4a8b2f05583546f02e7b998
Larry Zhu <larry.l.zhu@outlook.com>
2025-08-05 23:49:25 -0400

    add identity support

--- README.md
+++ README.md
@@ -4,14 +4,18 @@ This package is used to query and ingest data into kusto
 
 ## To Install
 
+Create a free kusto cluster following these instructions, https://learn.microsoft.com/en-us/azure/data-explorer/start-for-free-web-ui
 ```
 py -m pip install net-kusto-client
+```
+
 Please create local.settings.json file in your home folder, you can also copy example.csv to home folder for testing.
+
 ~/
 ├── local.settings.json
 ├── example.csv
-```
 
+The client_id in local.settings.json can be either the aad application id or the user-assigned managed identity. The client_secret is used as the application key.
 ## Usage
 ```
 import net_kusto_client
--- src/net_kusto_client/__init__.py
+++ src/net_kusto_client/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from azure.kusto.data import KustoConnectionStringBuilder, KustoClient
 from azure.kusto.data.data_format import DataFormat
 from azure.kusto.data.exceptions import KustoServiceError
-from azure.kusto.ingest import QueuedIngestClient, IngestionProperties, FileDescriptor, BlobDescriptor
+from azure.kusto.ingest import QueuedIngestClient, IngestionProperties, FileDescriptor
 from azure.kusto.data.helpers import dataframe_from_result_table
 
 HOME_DIR = Path(os.path.expanduser('~'))
@@ -23,20 +23,29 @@ INGESTION_URI = jdata['Values']['INGESTION_URI']
 KUSTO_URI = f"https://{CLUSTER_URI}.{REGION}.kusto.windows.net"
 KUSTO_INGEST_URI = f"https://ingest-{INGESTION_URI}.{REGION}.kusto.windows.net"
 KUSTO_DATABASE = jdata['Values']['KUSTO_DATABASE']
-KUSTO_TABLE = jdata['Values']['KUSTO_TABLE']
 
-KCSB_INGEST_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_INGEST_URI, CLIENT_ID, CLIENT_SECRET, AUTHORITY_ID)
-KCSB_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_URI, CLIENT_ID, CLIENT_SECRET, AUTHORITY_ID)
 
+if CLIENT_SECRET:
+    KCSB_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_URI, CLIENT_ID,
+                                                                                     CLIENT_SECRET, AUTHORITY_ID)
+    KCSB_INGEST_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_INGEST_URI, CLIENT_ID,
+                                                                                            CLIENT_SECRET, AUTHORITY_ID)
+else:
+    KCSB_DATA = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(KUSTO_URI,
+                                                                                              client_id=CLIENT_ID)
+    KCSB_INGEST_DATA = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(KUSTO_INGEST_URI,
+                                                                                                     client_id=CLIENT_ID)
 # My example
 EXAMPLE_KCSB_DATA = KustoConnectionStringBuilder.with_interactive_login(KUSTO_URI)
 EXAMPLE_INGEST_KCSB_DATA = KustoConnectionStringBuilder.with_interactive_login(KUSTO_INGEST_URI)
 EXAMPLE_FILE = Path(HOME_DIR) / "example.csv"
+EXAMPLE_KUSTO_TABLE = jdata['Values']['EXAMPLE_KUSTO_TABLE']
 
-# Store Events Example
+# Storm Events Example
 SAMPLE_KUSTO_URI = "https://help.kusto.windows.net/"
 SAMPLE_KUSTO_DATABASE = "Samples"
 SAMPLE_KCSB = KustoConnectionStringBuilder.with_interactive_login(SAMPLE_KUSTO_URI)
+
 
 class NetKustoClient:
     def __init__(self):
@@ -45,11 +54,55 @@ class NetKustoClient:
         self.example_client = KustoClient(EXAMPLE_KCSB_DATA)
         self.example_ingest_client = QueuedIngestClient(EXAMPLE_INGEST_KCSB_DATA)
         self.storm_events_sample_client = KustoClient(SAMPLE_KCSB)
+    
+    def create_table(self, table_name):
+        try:
+            CREATE_TABLE_COMMAND  = f""".create table {table_name} (DeviceName: string, OSVersion: string)"""
+            RESPONSE = self.example_client.execute_mgmt(KUSTO_DATABASE, CREATE_TABLE_COMMAND)
+            dataframe_from_result_table(RESPONSE.primary_results[0])
+        except KustoServiceError as err:
+            print("Error creating table:", err)
+    
+    def ingest_data(self, table_name, file_path):
+        try:
+            if table_name:
+                ingestion_properties = IngestionProperties(
+                    database=KUSTO_DATABASE,
+                    table=table_name,
+                    data_format=DataFormat.CSV,
+                )
+                if not os.path.isfile(file_path):
+                    print(f"File {file_path} does not exist.")
+                    return
+                file_descriptor = FileDescriptor(str(file_path))
+                self.example_ingest_client.ingest_from_file(file_descriptor, ingestion_properties)
+                print(f"Ingestion of {file_path} to table deviceinfo in database {KUSTO_DATABASE} initiated successfully.")
+        except KustoServiceError as err:
+            print("Error ingesting data:", err)
+    
+    def execute_take_ten_query(self, table_name):
+        try:
+            if table_name:
+                query = f"{table_name} | take 10"
+                response = self.example_client.execute_query(KUSTO_DATABASE, query)
+                df = dataframe_from_result_table(response.primary_results[0])
+                return df
+        except KustoServiceError as err:
+            print("Error executing query:", err)
+    
+    def execute_query(self, query):
+        try:
+            if query:
+                response = self.example_client.execute_query(KUSTO_DATABASE, query)
+                df = dataframe_from_result_table(response.primary_results[0])
+                return df
+        except KustoServiceError as err:
+            print("Error executing query:", err)
 
     def create_sample_table(self):
         try:
-            CREATE_TABLE_COMMAND  = f""".create table {KUSTO_TABLE} (DeviceName: string, OSVersion: string)"""
-            RESPONSE = self.example_client.execute_mgmt(KUSTO_DATABASE, CREATE_TABLE_COMMAND )
+            CREATE_TABLE_COMMAND  = f""".create table {EXAMPLE_KUSTO_TABLE} (DeviceName: string, OSVersion: string)"""
+            RESPONSE = self.example_client.execute_mgmt(KUSTO_DATABASE, CREATE_TABLE_COMMAND)
             dataframe_from_result_table(RESPONSE.primary_results[0])
         except KustoServiceError as err:
             print("Error creating table:", err)
@@ -58,7 +111,7 @@ class NetKustoClient:
         try:
             ingestion_properties = IngestionProperties(
                 database=KUSTO_DATABASE,
-                table=KUSTO_TABLE,
+                table=EXAMPLE_KUSTO_TABLE,
                 data_format=DataFormat.CSV,
             )
             file_descriptor = FileDescriptor(str(EXAMPLE_FILE))
@@ -69,7 +122,7 @@ class NetKustoClient:
 
     def execute_sample_query(self):
         try:
-            query = f"{KUSTO_TABLE} | take 10"
+            query = f"{EXAMPLE_KUSTO_TABLE} | take 10"
             response = self.example_client.execute_query(KUSTO_DATABASE, query)
             df = dataframe_from_result_table(response.primary_results[0])
             print(df)
